e29224c58e24a6a2d5d9c2f0e8304e2f4654a2f3
ndtThinh <thinhndt1001@gmail.com>
2023-04-21 11:31:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.study;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTabLayout=findViewById(R.id.tabLayout);
+        mViewPager=findViewById(R.id.viewPager);
+
+        ViewPagerAdapter viewPagerAdapter= new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mViewPager.setAdapter(viewPagerAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+}
---
+++ app/src/main/java/com/example/study/ViewPagerAdapter.java
@@ -0,0 +1,52 @@
+package com.example.study;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return new FavoriteFragment();
+            case 2:
+                return new MyPageFragment();
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title="";
+        switch (position){
+            case 0:
+                title="Home";
+                break;
+            case 1:
+                title="Favorite";
+                break;
+            case 2:
+                title="My page";
+                break;
+        }
+        return title;
+    }
+}
